e0fc9f35aa264d9f977ff02bd3af0a581fc8fa9a
olomonos <o.lomonos@gmail.com>
2019-01-08 17:52:33 +0200

    finished lesson 13;

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
-
-// Do work here
-router.get('/', storeController.homePage);
+const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/reverse/:name', (req, res) => {
   const reverse = [...req.params.name].reverse().join('');
   res.send(reverse);
 });
+
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
+router.get('/add', storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
