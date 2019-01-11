5f2a7705c3e652de81b7c6fe2641c1081ecbc455
olomonos <o.lomonos@gmail.com>
2019-01-11 16:07:59 +0200

    lesson 18;

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,19 @@ router.get('/reverse/:name', (req, res) => {
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
-router.post('/add', catchErrors(storeController.createStore));
-router.post('/add/:id', catchErrors(storeController.updateStore));
+
+router.post('/add',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.createStore),
+);
+
+router.post('/add/:id',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.updateStore)
+);
+
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 module.exports = router;
