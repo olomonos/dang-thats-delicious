4b586e610ec20d6b273934ad76890b0fceab8802
olomonos <o.lomonos@gmail.com>
2019-01-16 16:37:16 +0200

    lesson 26;

--- routes/index.js
+++ routes/index.js
@@ -48,4 +48,10 @@ router.post('/register',
 
 router.get('/logout', authController.logout);
 
+router.get('/account',
+  authController.isLoggedIn,
+  userController.account,
+);
+router.post('/account', catchErrors(userController.updateAccount));
+
 module.exports = router;
