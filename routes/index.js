d03983e6b77e241e488975a4234e15e8c0b6545c
olomonos <o.lomonos@gmail.com>
2019-01-16 16:06:16 +0200

    lesson 25;

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,10 @@ router.get('/reverse/:name', (req, res) => {
 
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/add', storeController.addStore);
+router.get('/add',
+  authController.isLoggedIn,
+  storeController.addStore,
+);
 
 router.post('/add',
   storeController.upload,
@@ -34,6 +37,7 @@ router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
 router.get('/register', userController.registerForm);
 
 router.post('/register',
@@ -41,5 +45,7 @@ router.post('/register',
   userController.register,
   authController.login,
 );
+
+router.get('/logout', authController.logout);
 
 module.exports = router;
